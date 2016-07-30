ab7af839ab1872565fa14872fe31c381a56244a5
erinlhiggins <elh76@pitt.edu>
2016-07-30 12:57:49 -0400

    milestone 3 submission

--- driver.java
+++ driver.java
@@ -1,4 +1,4 @@
-//jdbc for project
+//Lena Ortmann and Erin Higgins final project driver
 
 import java.sql.*;  
 import java.text.ParseException;
@@ -21,15 +21,14 @@ public class driver {
 	public static void createUser(int casenum){
 		try{
 	
-	    connection.setAutoCommit(false); //the default is true and every statement executed is considered a transaction.
+		    connection.setAutoCommit(false); //the default is true and every statement executed is considered a transaction.
 	    connection.setTransactionIsolation(Connection.TRANSACTION_READ_COMMITTED);
 	    query = "insert into Profiles values (?,?,?,?,?,?)";
 	    prepStatement = connection.prepareStatement(query);
 		
 		
-	
 		String user = "Zachary Quinto";
-		String email = "zq95@gmail.com";
+		String email = "zq95@gmail";
 		
 		
 	    java.text.SimpleDateFormat df = new java.text.SimpleDateFormat("yyyy-MM-dd");
@@ -46,7 +45,23 @@ public class driver {
 		prepStatement.setTimestamp(5,lastlogin);
 		prepStatement.setString(6,group);
 	    
-	    prepStatement.executeUpdate();
+	   prepStatement.executeUpdate();
+	   
+
+	  query ="select * from Profiles where userID = ?";
+	  
+	   prepStatement = connection.prepareStatement(query);
+	   int userID = 200;
+	   prepStatement.setInt(1, userID);
+	   
+	    ResultSet resultSet = prepStatement.executeQuery();
+	   System.out.println("userID" + " " + "nameofuser" + " " + "email" + " " + "dob" + " " + "timeoflastlogin" + " " + "ingroup");
+		
+		while(resultSet.next())
+		{
+	    System.out.println(resultSet.getInt(1)+"   "+ resultSet.getString(2)+"   "+resultSet.getString(3)+"   "+resultSet.getDate(4)+"   "+resultSet.getDate(5) + "   " + resultSet.getString(6));
+		}
+	    resultSet.close();
 		connection.commit();
 		}
 	catch(SQLException Ex) {
@@ -75,12 +90,9 @@ public class driver {
 	    connection.setTransactionIsolation(Connection.TRANSACTION_READ_COMMITTED);
 	    query = "insert into Friendships values (?,?,?,?,?)";
     	prepStatement = connection.prepareStatement(query);
-    	
-    	Scanner keyboard = new Scanner(System.in);
-		System.out.println("Your username:");
-		String name1 = keyboard.nextLine();
-		System.out.println("Friend to request:");
-		String name2 = keyboard.nextLine();
+
+		String name1 = "Erin Higgins";
+		String name2 = "Zachary Quinto";
 		
 		String status = "pending";
 	    long request = 0;
@@ -118,18 +130,13 @@ public class driver {
 
 	public static void establishFriendship(int casenum){
 		try{
-            connection.setAutoCommit(false); //the default is true and every statement executed is considered a transaction.
+                 connection.setAutoCommit(false); //the default is true and every statement executed is considered a transaction.
 	    connection.setTransactionIsolation(Connection.TRANSACTION_READ_COMMITTED);
 		  query = "update Friendships set status = ?, request = ? WHERE nameofuser = ?";
 		  prepStatement = connection.prepareStatement(query);
-		  //System.out.println("here1");
 		  
-		  
-		  Scanner keyboard = new Scanner(System.in);
-		System.out.println("Your username:");
-		String name1 = keyboard.nextLine();
-		System.out.println("Accept request to:");
-		String name2 = keyboard.nextLine();
+		String name1 = "Erin Higgins";
+		String name2 = "Zachary Quinto";
 		
 		String status = "Friends";
 	    int request = 0;
@@ -144,7 +151,7 @@ public class driver {
 	   
 	    prepStatement.executeUpdate();
 		
-	connection.commit();	
+	   connection.commit();		
 			
 		}
 		catch(Exception Ex)  
@@ -169,9 +176,7 @@ public class driver {
 	 connection.setTransactionIsolation(Connection.TRANSACTION_READ_COMMITTED);
 	   query ="select * from Friendships where nameofuser = ?";
 	   prepStatement = connection.prepareStatement(query);
-	   Scanner keyboard = new Scanner(System.in);
-	   System.out.println("Your username:");
-	   String name1 = keyboard.nextLine();
+	   String name1 = "Erin Higgins";
 	   prepStatement.setString(1, name1);
 	   
 	    ResultSet resultSet = prepStatement.executeQuery();
@@ -203,26 +208,36 @@ public class driver {
 	//Function 5 : CreateGroup
 public static void createGroup(int casenum){
 	try{
-	 connection.setAutoCommit(false); //the default is true and every statement executed is considered a transaction.
+		 connection.setAutoCommit(false); //the default is true and every statement executed is considered a transaction.
 	 connection.setTransactionIsolation(Connection.TRANSACTION_READ_COMMITTED);	
 		query = "insert into Groups values(?,?,?,?)";
 		
 		prepStatement = connection.prepareStatement(query);
       
-        Scanner keyboard = new Scanner(System.in);
-	   System.out.println("Group name:");
-	   String subject = keyboard.nextLine();
-	   System.out.println("Group Description:");
-	    String description = keyboard.nextLine();
-	    System.out.println("Membership limit:");
-	    int memberlimit = keyboard.nextInt();
+	    String groupname = "FFA";
+	    String description = "Future Farmers of America";
+	    int memberlimit = 2;
 		int members = 0;
 	
-	    prepStatement.setString(1, subject); 
+	    prepStatement.setString(1, groupname); 
 	    prepStatement.setString(2, description);
 	    prepStatement.setInt(3, members);
 		prepStatement.setInt(4,memberlimit);
 	    prepStatement.executeUpdate();
+		
+		
+			query ="select * from Groups where groupname = ?";
+	  
+	   prepStatement = connection.prepareStatement(query);
+	   prepStatement.setString(1, groupname);
+		 ResultSet resultSet = prepStatement.executeQuery();
+	   System.out.println("groupname" + " " + "description" + " " + "numofmembers" + " " + "membershiplimit");
+		
+		while(resultSet.next())
+		{
+	    System.out.println(resultSet.getString(1)+"   "+ resultSet.getString(2)+"   "+resultSet.getInt(3) + " " +  resultSet.getInt(4));
+		}
+	    resultSet.close();
 		
 		connection.commit();	
 		}
@@ -246,13 +261,12 @@ public static void createGroup(int casenum){
     public static void addToGroup(int casenum) {
 	
 	try {
+	
+	if(casenum == 0){
     	 connection.setAutoCommit(false); //the default is true and every statement executed is considered a transaction.
 	 connection.setTransactionIsolation(Connection.TRANSACTION_READ_COMMITTED);
-    	Scanner keyboard = new Scanner(System.in);
-		System.out.println("Username:");
-		String name1 = keyboard.nextLine();
-		System.out.println("Group:");
-		String name2 = keyboard.nextLine();
+		String name1 = "Erin Higgins";
+		String name2 = "FFA";
 	    
 	    query = "update Profiles set ingroup = ? where username = ?";
 	    prepStatement = connection.prepareStatement(query);
@@ -265,6 +279,44 @@ public static void createGroup(int casenum){
 		prepStatement.setString(1, name2); 
 	    prepStatement.executeUpdate();
 	    connection.commit();
+	    }
+	    
+	if(casenum == 1){
+	connection.setAutoCommit(false); //the default is true and every statement executed is considered a transaction.
+	 connection.setTransactionIsolation(Connection.TRANSACTION_READ_COMMITTED);
+		String name1 = "Zachary Quinto";
+		String name2 = "FFA";
+	    
+	    query = "update Profiles set ingroup = ? where username = ?";
+	    prepStatement = connection.prepareStatement(query);
+	    prepStatement.setString(1, name2);
+	    prepStatement.setString(2, name1);
+	    prepStatement.executeUpdate();
+	    
+	    query = "update Groups set numofmembers = numofmembers + 1 where groupname = ?";
+    	prepStatement = connection.prepareStatement(query);
+		prepStatement.setString(1, name2); 
+	    prepStatement.executeUpdate();
+	    connection.commit();
+	}
+    if(casenum == 2){
+        connection.setAutoCommit(false); //the default is true and every statement executed is considered a transaction.
+	 connection.setTransactionIsolation(Connection.TRANSACTION_READ_COMMITTED);
+		String name1 = "Gabby Means";
+		String name2 = "FFA";
+	    
+	    query = "update Profiles set ingroup = ? where username = ?";
+	    prepStatement = connection.prepareStatement(query);
+	    prepStatement.setString(1, name2);
+	    prepStatement.setString(2, name1);
+	    prepStatement.executeUpdate();
+	    
+	    query = "update Groups set numofmembers = numofmembers + 1 where groupname = ?";
+    	prepStatement = connection.prepareStatement(query);
+		prepStatement.setString(1, name2); 
+	    prepStatement.executeUpdate();
+	    connection.commit();
+    }
 	}	
 	catch(Exception Ex)  {
 	    System.out.println("Machine Error: " +
@@ -284,19 +336,15 @@ public static void createGroup(int casenum){
 //Function 7 : sendMessageTouser
 public static void sendMessageTouser(int casenum){
 	try{
-        connection.setAutoCommit(false); //the default is true and every statement executed is considered a transaction.
+         connection.setAutoCommit(false); //the default is true and every statement executed is considered a transaction.
 	    connection.setTransactionIsolation(Connection.TRANSACTION_READ_COMMITTED);
 		query = "insert into Messages values(?,?,?,?,?,?)";
 		prepStatement = connection.prepareStatement(query);
-		Scanner keyboard = new Scanner(System.in);
-	   System.out.println("Subject:");
-	   String subject = keyboard.nextLine();
-	   System.out.println("Message:");
-	   String textmsg = keyboard.nextLine();
-	   System.out.println("Your username:");
-	   String sender = keyboard.nextLine();
-	   System.out.println("Recipient:");
-	   String recipient = keyboard.nextLine();
+		
+	   String subject = "Food";
+	   String textmsg = "I love Perogies";
+	   String sender = "Joe Lynch";
+	   String recipient = "Erin Higgins";
 		java.text.SimpleDateFormat df = new java.text.SimpleDateFormat("yyyy-MM-dd");
 	    java.sql.Date datereg = new java.sql.Date (df.parse("2016-05-13").getTime());
 		int groupmsg = 0;
@@ -308,6 +356,8 @@ public static void sendMessageTouser(int casenum){
 		prepStatement.setDate(5,datereg);
 		prepStatement.setInt(6,groupmsg);
 		prepStatement.executeUpdate();
+				
+				
 				
 		connection.commit();
 		}
@@ -338,16 +388,11 @@ public static void sendMessageTouser(int casenum){
 	    connection.setTransactionIsolation(Connection.TRANSACTION_READ_COMMITTED);
 	    query = "select username from Profiles where ingroup = ?";
 	    prepStatement = connection.prepareStatement(query);
-	    
-	    Scanner keyboard = new Scanner(System.in);
-	    System.out.println("Enter your username: ");
-	    String user = keyboard.nextLine();
-		System.out.println("Group to send to:");
-		String name1 = keyboard.nextLine();
-		System.out.println("Subject: ");
-		String sub = keyboard.nextLine();
-		System.out.println("Message: ");
-		String message = keyboard.nextLine();
+
+	    String user = "Gabby Means";
+		String name1 = "FFA";
+		String sub = "Joining";
+		String message = "Please allow more members into your group";
 		
 		prepStatement.setString(1, name1);
 		
@@ -394,9 +439,7 @@ public static void displayMessages(int casenum){
 		query = "select * from Messages where recipient = ?";
     	prepStatement = connection.prepareStatement(query);
     	
-    	Scanner keyboard = new Scanner(System.in);
-		System.out.println("recipient:");
-		String name1 = keyboard.nextLine();
+		String name1 = "Erin Higgins";
 		
 		prepStatement.setString(1, name1); 
 	    
@@ -435,10 +478,8 @@ catch(SQLException Ex) {
 	    connection.setTransactionIsolation(Connection.TRANSACTION_READ_COMMITTED);
 		query = "select * from Messages where recipient = ? AND dateofmsg > (select timeoflastlogin from Profiles where username = ?)";
     	prepStatement = connection.prepareStatement(query);
-    	
-    	Scanner keyboard = new Scanner(System.in);
-		System.out.println("Username:");
-		String name1 = keyboard.nextLine();
+
+		String name1 = "Erin Higgins";
 		
 		prepStatement.setString(1, name1); 
 	    prepStatement.setString(2, name1);
@@ -477,9 +518,17 @@ Scanner scanner = new Scanner(System.in);
 		query = "select * from Profiles where username LIKE ? OR email LIKE ?";
 		prepStatement = connection.prepareStatement(query);
 		Scanner keyboard = new Scanner(System.in);
-		System.out.println("Search for:");
-		String name1 = keyboard.nextLine();
+		String name1 = "start";
 		
+		if(casenum == 0)
+		{
+		name1 = "Zac";
+		}
+		
+		if(casenum == 1)
+		{
+		name1 = "zq";
+		}
 		prepStatement.setString(1, "%"+name1+"%");
 		prepStatement.setString(2, "%"+name1+"%");
 		
@@ -532,11 +581,24 @@ Scanner scanner = new Scanner(System.in);
 		query = "select requestto from Friendships where (nameofuser = ? AND requestto = ?) OR (nameofuser = ? AND requestto = ?) AND status = 'Friends'";
 		prepStatement = connection.prepareStatement(query);
 		
-		Scanner keyboard = new Scanner(System.in);
-		System.out.println("Username:");
-		String name1 = keyboard.nextLine();
-		System.out.println("Other Person:");
-		String name2 = keyboard.nextLine();
+		String name1 = "start";
+		String name2 = "start";
+		
+		if(casenum == 0)
+		{
+		name1 = "Joe Lynch";
+		name2 = "Erin Higgins";
+		}
+		if(casenum == 1)
+		{
+		name1 = "Joe Lynch";
+		name2 = "Maggie Devine";
+		}
+		if(casenum == 2)
+		{
+		name1 = "Joe Lynch";
+		name2 = "David Spade";
+		}
 		prepStatement.setString(1, name1);
 		prepStatement.setString(2, name2);
 		prepStatement.setString(3, name2);
@@ -644,10 +706,14 @@ public static void topMessages(int x, int k)
 		try {
 		 connection.setAutoCommit(false); //the default is true and every statement executed is considered a transaction.
 	         connection.setTransactionIsolation(Connection.TRANSACTION_READ_COMMITTED);
+	         String name1 = "start";
 			
-			Scanner keyboard = new Scanner(System.in);
-			System.out.println("Username to drop:");
-			String name1 = keyboard.nextLine();
+			if(casenum == 0){
+			name1 = "Joe Lynch";
+			}
+			if(casenum == 1){
+			name1 = "Erin Higgins";
+			}
 			
 			query = "delete from Friendships where nameofuser = ? OR requestto = ?";
 			prepStatement = connection.prepareStatement(query);
@@ -742,7 +808,7 @@ public static void topMessages(int x, int k)
 	    createUser(0);          // create user
 	    System.out.println("Successfully added");
 	    System.out.println("Trying to add Zachary Quinto again");
-	    createUser(1);          // try to create user that already exists
+	    createUser(0);          // try to create user that already exists
 	    System.out.println("Using initaiteFriendship");
 	    System.out.println("Initiating friendship between Erin Higgins and Zachary Quinto");
 	    initiateFriendship(0);  // initiate a friendship between newly created user and another person
@@ -765,7 +831,7 @@ public static void topMessages(int x, int k)
 	    System.out.println("Trying to add Gabby Means to group");
 	    addToGroup(2);          // too many people
 	    System.out.println("Using sendMessageTouser");
-	    System.out.println("Sending message from Joe Lynch to Kalyn Sigg");
+	    System.out.println("Sending message from Joe Lynch to Erin Higgins");
 	    sendMessageTouser(0);   // send message to one person
 	    System.out.println("Using displayMessages to prove it works");
 	    displayMessages(0);     // show the messages to prove
@@ -780,8 +846,6 @@ public static void topMessages(int x, int k)
 	    System.out.println("Use search to try and find Zachary");
 	    System.out.println("Searching for: Zac");
 	    searchForUser(0);       // search for user name of new user added at beginning
-	    System.out.println("Searching for: zq");
-	    searchForUser(1);       // search for email
 	    System.out.println("Using threeDegrees");
 	    System.out.println("Show relationship between Joe Lynch and Erin Higgins");
 	    threeDegrees(0);        // show that direct relationships can be found
@@ -793,8 +857,16 @@ public static void topMessages(int x, int k)
 	    topMessages(12, 12);         // show last year
 	    System.out.println("Show top message receivers for the past 144 months");
 	    topMessages(144, 12);         // show last couple years
-	    System.out.println("Drop Zachary Quinto");
+	    System.out.println("Showing Zachary's messages, groups, and friendships before dropping");
+	    System.out.println("Drop Joe Lynch");
 	    dropUser(0);            // drop user then show that he is not found in any of the places that he was in before including messages
+	    System.out.println("Showing that messages are still in there from Joe if the other user is still in the db");
+	    displayMessages(0);
+	    System.out.println("Dropping Erin Higgins");
+	    dropUser(1);
+	    System.out.println("Showing that these messages are now gone");
+	    displayMessages(0);
+	    
 	    
 	}
 	catch(Exception Ex)  {
